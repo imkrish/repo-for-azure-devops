92a3fa08ece1e0ac0f36941b8849994591a6f278
Keerati Limkulphong <imkrish.developer@gmail.com>
2020-01-11 11:07:17 +1300

    Add failed test

--- src/utils/number.operator.test.ts
+++ src/utils/number.operator.test.ts
@@ -3,3 +3,7 @@ import { add } from './number-operator'
 test('add 1 and 2 to equal 3', () => {
   expect(add(1)(2)).toBe(3)
 })
+
+test('add 2 and 4 to equal 6', () => {
+  expect(add(2)(4)).toBe(3)
+})
