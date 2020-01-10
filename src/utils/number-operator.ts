32818a3bfba9c9160f59de880837721fb4dafd87
Keerati Limkulphong <imkrish.developer@gmail.com>
2020-01-11 11:57:37 +1300

    add code

--- src/utils/number-operator.ts
+++ src/utils/number-operator.ts
@@ -6,6 +6,4 @@ export const minus = (num1: number) => (num2: number) => {
   return num1 - num2
 }
 
-export const print = () => {
-  console.log('print ja')
-}
+export const print = () => console.log('print ja')
