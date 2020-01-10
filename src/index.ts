246f8cbf8e5679049e23d0a67b068deae2368210
Keerati Limkulphong <imkrish.developer@gmail.com>
2020-01-11 11:53:04 +1300

    add more code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { add } from './utils/number-operator'
+import { add, minus, print } from './utils/number-operator'
 
 async function main() {
   const result = add(1)(2)
   console.log(result)
+  minus(2)(1)
+  print()
 }
 
 main()
