9ec1164108e5bafc6ae35ee16d4321c4ce0085a8
Keerati Limkulphong <imkrish@Keeratis-MacBook-Pro.local>
2020-01-12 09:54:48 +1300

    Analyze code using SonarCloud

--- src/utils/number-operator.ts
+++ src/utils/number-operator.ts
@@ -1,11 +1,12 @@
 export class NumberOperator {
   static add = (num1: number) => (num2: number) => {
-    return num1 + num2
-  }
+    var x = 1;
+    return num1 + num2;
+  };
 
-  static minus = (num1: number) => (num2: number) => num1 - num2
+  static minus = (num1: number) => (num2: number) => num1 - num2;
 
   static print = () => {
-    console.log('hehe')
-  }
+    console.log("hehe");
+  };
 }
